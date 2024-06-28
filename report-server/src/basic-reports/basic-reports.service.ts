62221d9fc1f3de0a3612b5f7fb926b089c7254e9
cjavat <cdpm98@hotmail.com>
2024-06-28 06:34:22 -0600

    SECCIÓN 3 / CAP 9 - EN CURSO

--- report-server/src/basic-reports/basic-reports.service.ts
+++ report-server/src/basic-reports/basic-reports.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrinterService } from '../printer/printer.service';
+import { getHelloWorldReport } from 'src/reports';
+import { getEmploymentLetterReport } from '../reports/employment-letter.report';
 
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
@@ -8,7 +11,21 @@ export class BasicReportsService extends PrismaClient implements OnModuleInit {
     // console.log('Conectado a la db');
   }
 
-  async hello() {
-    return this.employees.findFirst();
+  constructor(private readonly printerService: PrinterService) {
+    super(); //? Se necesita llamar para inicializar lo de prisma
+  }
+
+  hello() {
+    const docDefinition = getHelloWorldReport({ name: 'CJavatX' });
+
+    const doc = this.printerService.createPdf(docDefinition);
+    return doc;
+  }
+
+  employmentLetter() {
+    const docDefinition = getEmploymentLetterReport();
+
+    const doc = this.printerService.createPdf(docDefinition);
+    return doc;
   }
 }
